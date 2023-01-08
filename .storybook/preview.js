895071a6144da447e7a85323210c5958c5cd4699
rafael <rafaelcnunes@icloud.com>
2023-01-08 19:27:57 -0300

    added Material Icons and Roboto

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
